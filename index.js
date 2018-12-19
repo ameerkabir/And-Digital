03c0babc85ffaa403be483a18042a8c076948a58
ameerkabir <ameerkabir2@gmail.com>
2018-12-19 12:35:55 +0000

    renamed some variables

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ function addCandidatesToTable(table, candidates) {
 
 function filterCandidateBySkill(candidates, skill) {
   console.log({ candidates }, { skill });
-  // Iterate over candidates array and return candidates with javascript skill
+  // Iterate over candidates array and return candidates with specific skill
 
   const filterBySkill = candidates.filter(candidate =>
     // Using includes will check every element of an  array and return the once that are meeting the condition.
@@ -46,29 +46,29 @@ function filterCandidateBySkill(candidates, skill) {
 }
 
 const candidatesTable = document.getElementById("candidates_example");
-const newCandidatesTable = candidatesTable.cloneNode(true);
+const withJavaScriptSkill = candidatesTable.cloneNode(true);
 const withPythonSkill = candidatesTable.cloneNode(true);
 const withAwsSkill = candidatesTable.cloneNode(true);
 
-removeRowsFromTable(newCandidatesTable);
+removeRowsFromTable(withJavaScriptSkill);
 removeRowsFromTable(withPythonSkill);
 removeRowsFromTable(withAwsSkill);
 
-const newTbody = newCandidatesTable.getElementsByTagName("tbody")[0];
+const newTbody = withJavaScriptSkill.getElementsByTagName("tbody")[0];
 const pythonTable = withPythonSkill.getElementsByTagName("tbody")[0];
 const AwsTable = withAwsSkill.getElementsByTagName("tbody")[0];
 
-const filteredCandidates = filterCandidateBySkill(newCandidates, "JavaScript");
+const candidateWithJavaScriptSkill = filterCandidateBySkill(newCandidates, "JavaScript");
 const candidateWithPythonSkill = filterCandidateBySkill(
   newCandidates,
   "Python"
 );
 const candidateWithAwsSkill = filterCandidateBySkill(newCandidates, "AWS");
 
-addCandidatesToTable(newTbody, filteredCandidates);
+addCandidatesToTable(newTbody, candidateWithJavaScriptSkill);
 addCandidatesToTable(pythonTable, candidateWithPythonSkill);
 addCandidatesToTable(AwsTable, candidateWithAwsSkill);
 
-document.body.appendChild(newCandidatesTable);
+document.body.appendChild(withJavaScriptSkill);
 document.body.appendChild(withPythonSkill);
 document.body.appendChild(withAwsSkill);
